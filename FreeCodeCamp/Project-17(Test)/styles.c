24e27a6d2f7c3865df54d5d1d1da954353203353
Muhammad Shuhaibh <147400263+shuhaibh@users.noreply.github.com>
2024-06-29 22:57:54 +0530

    Create styles.c

---
+++ FreeCodeCamp/Project-17(Test)/styles.c
@@ -0,0 +1,24 @@
+*,
+::before,
+::after {
+  padding: 0;
+  margin: 0;
+box-sizing:border-box;
+}
+
+HTML{
+  font-size: 62.5%; 
+}
+
+body {
+  font-family: 'Baskervville', serif;
+  color: linen;
+  background-color: #FDFCDC;
+}
+
+#header{
+  float:top;
+  width:100%;
+  height:7rem;
+  background-color:#0081A7;
+}
